e34966c572068ad007eef184234e2de1a41ac950
StrawbErika <erika.nepomuceno392@gmail.com>
2020-09-18 16:05:40 +0800

    Changed class component Login to functional component

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -6,25 +6,23 @@ import "firebase/auth";
 // const firebase = useFirebaseApp();
 
 //TODO convert this to a functional component to use firebase hooks
-class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: "",
-      error: "",
-    };
-  }
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  handleChange = (e) => {
-    this.setState({
-      ...this.state,
-      [e.target.name]: e.target.value,
-      error: "",
-    });
+  const handleChange = (e) => {
+    setError("");
   };
+  // handleChange = (e) => {
+  //     this.setState({
+  //       ...this.state,
+  //       [e.target.name]: e.target.value,
+  //       error: "",
+  //     });
+  //   };
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     // firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
     //     .then(result => {
@@ -44,32 +42,27 @@ class Login extends Component {
     //         })
     //     })
   };
-
-  render() {
-    return (
-      <>
-        <h1>Log In</h1>
-        <form onSubmit={() => alert("AGHHHH I CANT FIX THIS HELP ME")}>
-          <input
-            type="text"
-            placeholder="Email"
-            name="email"
-            onChange={this.handleChange}
-          />
-          <br />
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            onChange={this.handleChange}
-          />
-          <br />
-          <button type="submit">Log in</button>
-        </form>
-        {this.state.error && <h4>{this.state.error}</h4>}
-      </>
-    );
-  }
+  return (
+    <>
+      <h1>Log In</h1>
+      <form onSubmit={() => alert("AGHHHH I CANT FIX THIS HELP ME")}>
+        <input
+          type="text"
+          placeholder="Email"
+          name="email"
+          onChange={handleChange}
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          onChange={handleChange}
+        />
+        <br />
+        <button type="submit">Log in</button>
+      </form>
+      {error && <h4>{error}</h4>}
+    </>
+  );
 }
-
-export default Login;
