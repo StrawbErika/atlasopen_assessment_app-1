9547e6e3d070e7c60d675269db84959feb967bf4
StrawbErika <erika.nepomuceno392@gmail.com>
2020-09-18 15:42:42 +0800

    Updated firebaseConfig, made folders for pages and moved the respective components into their own page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import React from 'react';
-import Signup from './Signup';
-import Login from './Login';
-import Chat from './Chat';
-import Logout from './Logout';
-import { useUser } from 'reactfire';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link, useParams } from 'react-router-dom';
+import React from "react";
+import Signup from "./Pages/Signup/Signup";
+import Login from "./Pages/Login/Login";
+import Chat from "./Pages/Chat/Chat";
+import Logout from "./Pages/Login/Logout";
+import { useUser } from "reactfire";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+} from "react-router-dom";
 
 //TODO: Add stats dashboard
 function App() {
   const user = useUser();
   return (
-      <div className="App">
-        {
-          user &&
-              <>
-              <Router>
-                  <Chat/>
-              </Router>
-              </>
-        }
-        {
-          !user &&
-          <>
-            <Signup />
-            <Login />
-          </>
-        }
-      </div>
+    <div className="App">
+      {user && (
+        <>
+          <Router>
+            <Chat />
+          </Router>
+        </>
+      )}
+      {!user && (
+        <>
+          <Signup />
+          <Login />
+        </>
+      )}
+    </div>
   );
 }
 
